87755eb7e3d187168478f68b931116b6931d9ae7
Ivan <lorf1991@gmail.com>
2021-08-29 20:44:54 +0300

    fixed 3hw

--- js/task-03.js
+++ js/task-03.js
@@ -1,26 +1,25 @@
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    alt: 'White and Black Long Fur Cat'
   },
   {
     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish'
   },
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
+    alt: 'Group of Horses Running'
+  }
 ];
 
-const makeGallary = ({url, alt}) => {
-  const gallaryImg = document.createElement('img');
-  gallaryImg.src = url;
-  gallaryImg.alt = alt;
+const makeGallary = ({url, alt}) => `
+  <li>
+    <img src=${url} alt=${alt}>
+  </li>`
+;
 
-  return gallaryImg;
-}
+const lists = images.map(makeGallary).join('');
 
-const imgs =  images.map(makeGallary);
 const gallary = document.querySelector('#gallery');
-gallary.append(...imgs);
+gallary.insertAdjacentHTML("afterbegin", lists);
